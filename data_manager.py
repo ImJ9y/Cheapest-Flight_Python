bb806dbd8c732babfd17a412bc54e69e517f645e
Jay Im <149026501+ImJ9y@users.noreply.github.com>
2024-11-11 17:52:20 -0800

    Add files via upload

--- data_manager.py
+++ data_manager.py
@@ -5,32 +5,44 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
-SHEETY_ENDPOINT = "https://api.sheety.co/4fe0fdf70f57a173dfb4963a13c125e6/flightDeals/prices"
-
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+
     def __init__(self):
         self._user = os.environ["SHEETY_USRERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
+        self.prices_endpoint = os.environ["SHEETY_PRICES_ENDPOINT"]
+        self.users_endpoint = os.environ["SHEETY_USERS_ENDPOINT"]
+
         self._authorization = HTTPBasicAuth(self._user, self._password)
         self.destination_data = {}
+        self.users_data = {}
 
-    def getData(self):
-        response = requests.get(url=SHEETY_ENDPOINT)
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=self.prices_endpoint)
         data = response.json()
         self.destination_data = data["prices"]
+        # Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
         return self.destination_data
 
-    def update_destination_data(self):
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
-                "price":{
+                "price": {
                     "iataCode": city["iataCode"]
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_ENDPOINT}/{city['id']}",
-                json = new_data
+                url=f"{self.prices_endpoint}/{city['id']}",
+                json=new_data
             )
             print(response.text)
+
+    def get_customer_emails(self):
+        response = requests.get(url=self.users_endpoint)
+        data = response.json()
+        self.users_data = data["users"]
+        return self.users_data
